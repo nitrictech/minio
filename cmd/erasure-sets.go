298dde5183047de1c9509e1ddb9e90a0996f1483
Tim Holm <tim.holm@nitric.io>
2022-11-21 15:31:26 +1100

    downgrade parity warning to Info.

--- cmd/erasure-sets.go
+++ cmd/erasure-sets.go
@@ -356,7 +356,7 @@ func newErasureSets(ctx context.Context, endpoints PoolEndpoints, storageDisks [
 	}
 
 	if defaultParityCount == 0 {
-		logger.Error("Warning: Default parity set to 0. This can lead to data loss.")
+		logger.Info("Warning: Default parity set to 0. This can lead to data loss.")
 	}
 
 	// Initialize the erasure sets instance.
